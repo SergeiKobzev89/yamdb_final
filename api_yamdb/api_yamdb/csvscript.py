65976eec2831b714cdeceffe1ce91b2249d6fa62
Kobzev Sergei <kobzevsergei89@yandex.ru>
2022-09-09 10:28:11 +0500

    Debug=True

--- api_yamdb/api_yamdb/csvscript.py
+++ api_yamdb/api_yamdb/csvscript.py
@@ -1,5 +1,5 @@
 import csv
-# from date import datetime as dt
+from date import datetime as dt
 import sqlite3
 
 conn = sqlite3.connect('../db.sqlite3')
@@ -19,21 +19,21 @@ with open("../static/data/category.csv", "r", encoding="utf8") as data:
         )
         conn.commit()
 
-# with open("../static/data/comments.csv", "r") as data:
-#    reader = csv.DictReader(data)
-#    for row in reader:
-#        id = row['id']
-#        review_id = row['review_id']
-#        text = row['text']
-#        author_id = row['author']
-#        pub_date = row['pub_date']
-#        c.execute(
-#            "INSERT INTO yamdb_comment "
-#            "(id, text, pub_date, author_id, review_id) "
-#            "VALUES(?, ?, ?, ?, ?)",
-#            (id, text, pub_date, author_id, review_id)
-#        )
-#        conn.commit()
+with open("../static/data/comments.csv", "r") as data:
+    reader = csv.DictReader(data)
+    for row in reader:
+        id = row['id']
+        review_id = row['review_id']
+        text = row['text']
+        author_id = row['author']
+        pub_date = row['pub_date']
+        c.execute(
+            "INSERT INTO yamdb_comment "
+            "(id, text, pub_date, author_id, review_id) "
+            "VALUES(?, ?, ?, ?, ?)",
+            (id, text, pub_date, author_id, review_id)
+        )
+        conn.commit()
 
 with open("../static/data/genre.csv", "r", encoding="utf8") as data:
     reader = csv.DictReader(data)
@@ -63,22 +63,22 @@ with open("../static/data/genre_title.csv", "r", encoding="utf8") as data:
         )
         conn.commit()
 
-# with open("../static/data/review.csv", "r", encoding="utf8") as data:
-#    reader = csv.DictReader(data)
-#    for row in reader:
-#        id = row['id']
-#        title_id = row['title_id']
-#        text = row['text']
-#        author_id = row['author']
-#        score = row['score']
-#        pub_date = row['pub_date']
-#        c.execute(
-#            "INSERT INTO yamdb_review "
-#            "(id, text, score, pub_date, author_id, title_id) "
-#            "VALUES(?, ?, ?, ?, ?, ?)",
-#            (id, text, score, pub_date, author_id, title_id)
-#        )
-#        conn.commit()
+with open("../static/data/review.csv", "r", encoding="utf8") as data:
+    reader = csv.DictReader(data)
+    for row in reader:
+        id = row['id']
+        title_id = row['title_id']
+        text = row['text']
+        author_id = row['author']
+        score = row['score']
+        pub_date = row['pub_date']
+        c.execute(
+            "INSERT INTO yamdb_review "
+            "(id, text, score, pub_date, author_id, title_id) "
+            "VALUES(?, ?, ?, ?, ?, ?)",
+            (id, text, score, pub_date, author_id, title_id)
+        )
+        conn.commit()
 
 with open("../static/data/titles.csv", "r", encoding="utf8") as data:
     reader = csv.DictReader(data)
@@ -95,38 +95,38 @@ with open("../static/data/titles.csv", "r", encoding="utf8") as data:
         )
         conn.commit()
 
-# with open("../static/data/users.csv", "r", encoding="utf8") as data:
-#    reader = csv.DictReader(data)
-#    for row in reader:
-#        id = row['id']
-#        password = '12345678'
-#        username = row['username']
-#        email = row['email']
-#        role = row['role']
-#        bio = row['description']
-#        first_name = row['first_name']
-#        last_name = row['last_name']
-#        date_joined = dt.now()
-#        if role == 'user':
-#            is_superuser = False
-#            is_staff = False
-#            is_active = True
-#        elif role == 'moderator':
-#            is_superuser = False
-#            is_staff = True
-#            is_active = True
-#        elif role == 'admin':
-#            is_superuser = True
-#            is_staff = False
-#            is_active = True
-#        c.execute(
-#            "INSERT INTO api_user "
-#            "(id, password, username, email, role, bio, first_name, "
-#            "last_name, is_superuser, is_staff, is_active, date_joined) "
-#            "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-#            (id, password, username, email, role, bio, first_name,
-#             last_name, is_superuser, is_staff, is_active, date_joined)
-#        )
-#        conn.commit()
+with open("../static/data/users.csv", "r", encoding="utf8") as data:
+    reader = csv.DictReader(data)
+    for row in reader:
+        id = row['id']
+        password = '12345678'
+        username = row['username']
+        email = row['email']
+        role = row['role']
+        bio = row['description']
+        first_name = row['first_name']
+        last_name = row['last_name']
+        date_joined = dt.now()
+        if role == 'user':
+            is_superuser = False
+            is_staff = False
+            is_active = True
+        elif role == 'moderator':
+            is_superuser = False
+            is_staff = True
+            is_active = True
+        elif role == 'admin':
+            is_superuser = True
+            is_staff = False
+            is_active = True
+        c.execute(
+            "INSERT INTO api_user "
+            "(id, password, username, email, role, bio, first_name, "
+            "last_name, is_superuser, is_staff, is_active, date_joined) "
+            "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            (id, password, username, email, role, bio, first_name,
+             last_name, is_superuser, is_staff, is_active, date_joined)
+        )
+        conn.commit()
 
 conn.close()
